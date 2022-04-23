be352922bc2c249b4c1f16569245cb908ccc966f
lyamz <davidlyangenda@gmail.com>
2022-04-23 14:15:05 +0200

    Deploy project to github pages

--- src/js/itemsCounter.js
+++
@@ -1,9 +0,0 @@
-export const itemsCounter = (data) => (typeof (data) === 'object' ? data.length : 'invalid');
-
-const displayTvShownumbers = (data) => {
-  const TvShowLink = document.querySelector('.tv-show-link');
-  const counter = itemsCounter(data);
-  TvShowLink.textContent = `TvShows(${counter})`;
-};
-
-export default displayTvShownumbers;
